a80b63333e6a0115ab3aebf4dc5c6b417614b483
harshitkhandelwal17 <101880873+harshitkhandelwal17@users.noreply.github.com>
2024-04-01 23:51:21 +0530

    Update environment.prod.ts

--- service-provider-main/frontend/src/environments/environment.prod.ts
+++ service-provider-main/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/v1/provider/',
+    apiUrl: 'https://service-provider-mean-stack-3.onrender.com/v1/provider/',
 };
